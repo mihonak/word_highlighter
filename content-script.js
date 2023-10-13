689acc3547cb705324ecd8065df4d80642501a44
miho nakayama <miho.nakayam@gmail.com>
2023-10-13 17:22:48 +0900

    css移行

--- content-script.js
+++ content-script.js
@@ -3,21 +3,13 @@ chrome.runtime.onMessage.addListener((request) => {
   const words = ["英語", "英会話", "TOEIC", "グローバル", "海外", "外国"];
 
   document.body.innerHTML =
-    '<div id="REOP-HighLightArea">' + document.body.innerHTML + "</div>";
-  let editedHTML = document.getElementById("REOP-HighLightArea");
-
-  const styleElm = document.createElement("style");
-  styleElm.innerHTML += "span.word-highlighter{background:#f99;}";
-  styleElm.innerHTML +=
-    ".REOP-ctrl{position:fixed;top:10px;right:10px;z-index:9998;width:200px;padding:0 10px 10px;text-align:left;color:#fff;background:#000;opacity:0.7;box-shadow: 0 3px 7px rgba(0, 0, 0, 0.6);border-radius:3px;}";
-  styleElm.innerHTML +=
-    ".REOP-ctrl p{margin:5px 0;}.REOP-ctrl input,.REOP-ctrl label{margin-right:5px;vertical-align:middle;}";
-  styleElm.innerHTML +=
-    ".REOP-ctrl div{margin:0 -10px 10px;padding:2px;background:#333;cursor:move;text-align:right;border-radius:3px;}.REOP-ctrl span{cursor:pointer;}";
-  document.head.appendChild(styleElm);
+    '<div id="word-highlighter-target-area">' +
+    document.body.innerHTML +
+    "</div>";
+  let editedHTML = document.getElementById("word-highlighter-target-area");
 
   const panelElm = document.createElement("div");
-  panelElm.className = "REOP-ctrl";
+  panelElm.className = "word-highlighter-panel";
   document.body.insertBefore(panelElm, editedHTML);
 
   class highlight {
@@ -29,7 +21,7 @@ chrome.runtime.onMessage.addListener((request) => {
       this.highlightOn();
     }
     highlightOn() {
-      const separator = "[REOP-array-separator]";
+      const separator = "[word-highlighter-array-separator]";
       const texts = this.htmlSrc.innerHTML
         .replace(new RegExp("(<)", "ig"), separator + "$1")
         .replace(new RegExp("(>)", "ig"), "$1" + separator)
@@ -50,7 +42,7 @@ chrome.runtime.onMessage.addListener((request) => {
             if (texts[i][j].substring(0, 1) != "&") {
               texts[i][j] = texts[i][j].replace(
                 new RegExp("(" + this.keyword + ")", "ig"),
-                '<span class="word-highlighter">$1</span>'
+                '<span class="word-highlighter-keywords">$1</span>'
               );
             }
           }
@@ -65,7 +57,7 @@ chrome.runtime.onMessage.addListener((request) => {
   words.map((word, index) => {
     patterns[index] = new highlight(
       word,
-      document.getElementById("REOP-HighLightArea")
+      document.getElementById("word-highlighter-target-area")
     );
   });
 
